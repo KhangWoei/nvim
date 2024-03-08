dcd44052eb3c8ed9d38457b82992497d6a889f64
kwc43 <41323253+kwc43@users.noreply.github.com>
2024-03-08 21:44:14 +0000

    move treesitter configuration to its own file
    also removed unused configurations

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,27 @@
 -- treesitter.lua
 
 return {
-  -- Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-  },
-  build = ':TSUpdate',
+    -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+
+
+    -- See `:help nvim-treesitter`
+    -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
+    config = vim.defer_fn(function()
+        require('nvim-treesitter.configs').setup {
+            -- Add languages to be installed here that you want installed for treesitter
+            ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc',
+                'vim',
+                'bash' },
+
+            auto_install = false,
+
+            highlight = { enable = true },
+            indent = { enable = true },
+        }
+    end, 0)
 }
