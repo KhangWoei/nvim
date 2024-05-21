b8eb101cf6e4d0c3c9c81318b5e5edcbba031472
kwc43 <41323253+kwc43@users.noreply.github.com>
2024-05-21 14:53:55 +0100

    configure indentation

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,10 +1,27 @@
 -- indent-blankline.lua
 -- Add indentation guides even on blank lines
 
-return {
-  'lukas-reineke/indent-blankline.nvim',
-  -- Enable `lukas-reineke/indent-blankline.nvim`
-  -- See `:help ibl`
-  main = 'ibl',
-  opts = {},
+local indent_blankline = {
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl'
 }
+
+local highlight = {
+    "CursorColumn",
+    "Whitespace",
+}
+
+function indent_blankline.config()
+    require("ibl").setup({
+        indent = { highlight = highlight, char = "" },
+        whitespace = {
+            highlight = highlight,
+            remove_blankline_trail = false,
+        },
+        scope = { enabled = false },
+    })
+end
+
+return indent_blankline
