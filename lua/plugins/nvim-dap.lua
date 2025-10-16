8ca23cffaabaa2544ff73903e98907c3ff5c887d
Khang Woei <41323253+KhangWoei@users.noreply.github.com>
2025-10-16 23:52:29 +0100

    add adapter for python + debug viewer

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -5,13 +5,24 @@ local nvim_dap = {
     "mfussenegger/nvim-dap",
     dependencies = {
         "mfussenegger/nvim-dap-python",
+        "igorlfs/nvim-dap-view",
     }
 }
 
 function nvim_dap.config()
     local dap_python = require('dap-python')
 
-    dap_python.setup('python')
+    local hatch_python = vim.fn.getcwd() .. '/.venv/bin/python'
+    if vim.fn.filereadable(hatch_python) == 1 then
+        dap_python.setup(hatch_python)
+    else
+        dap_python.setup('python3')
+    end
+
+    local dap_view = require('dap-view')
+    dap_view.setup({
+        auto_toggle = true
+    })
 end
 
 function nvim_dap.keys()
@@ -19,9 +30,9 @@ function nvim_dap.keys()
 
     return {
         { "<F5>",       dap.continue,                                                              desc = "DAP: Continue" },
-        { "<F10>",      dap.step_over,                                                             desc = "DAP: Step Over" },
-        { "<F11>",      dap.step_into,                                                             desc = "DAP: Step Into" },
-        { "<F12>",      dap.step_out,                                                              desc = "DAP: Step Out" },
+        { "<F6>",       dap.step_over,                                                             desc = "DAP: Step Over" },
+        { "<F7>",       dap.step_into,                                                             desc = "DAP: Step Into" },
+        { "<F8>",       dap.step_out,                                                              desc = "DAP: Step Out" },
         { "<leader>b",  dap.toggle_breakpoint,                                                     desc = "Toggle [B]reakpoint" },
         { "<leader>bc", function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Togle [B]reakpoint, [C]onditional" },
     }
